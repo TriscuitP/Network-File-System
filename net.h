b55f3669c9a8969d9e51407f598576c6d43b917c
Tristan Perez <31454023+TriscuitP@users.noreply.github.com>
2018-05-13 00:37:13 -0700

    Update net.h

--- net.h
+++ net.h
@@ -3,11 +3,14 @@
 
 #include <unistd.h>
 #include <stdint.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 
 enum msg_types {
     MSG_READDIR = 1, 
-    MSG_GETATTR = 2
+    MSG_GETATTR = 2,
+    MSG_OPEN = 3,
+    MSG_READ = 4
 };
 
 struct __attribute__((__packed__)) netfs_msg_header {
@@ -17,12 +20,14 @@ struct __attribute__((__packed__)) netfs_msg_header {
 
 struct attr_stat
 {
-    ino_t ino;           /* Inode number */
-    uid_t uid;           /* User ID of owner */
-    gid_t gid;           /* Group ID of owner */
-    mode_t mode;         /* File type and mode */
-    nlink_t nlink;       /* Number of hard links */
-    off_t size;          /* Total size, in bytes */
+	ino_t ino;	            /* Inode number */
+    uid_t uid;              /* User ID of owner */
+    gid_t gid;              /* Group ID of owner */
+    mode_t mode;            /* File type and mode */
+    nlink_t nlink;          /* Number of hard links */
+    off_t size;             /* Total size, in bytes */
+    blkcnt_t blocks;        /* Number of 512B blocks allocated */
+    struct timespec mtim;   /* Time of last modification */
 };
 
 int connect_to(char *hostname, int port);
