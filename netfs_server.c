528b8da212834358014d0d15053a746b787965a1
Tristan Perez <31454023+TriscuitP@users.noreply.github.com>
2019-05-31 10:39:46 -0700

    Update netfs_server.c

--- netfs_server.c
+++ netfs_server.c
@@ -22,13 +22,18 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <pwd.h>
+#include <semaphore.h>
 
 #include "common.h"
 #include "logging.h"
 #include "net.h"
 
-char* home_path;
+sem_t thread_semaphore;
 
+/*
+ * Handles each request from client 
+ */
 void handle_request(int client_fd) 
 {
     struct netfs_msg_header req_header = { 0 };
@@ -40,88 +45,48 @@ void handle_request(int client_fd)
     uint16_t type = req_header.msg_type;
     LOG("req_header type: %d\n", type);
 
-
-    int status;
-    pid_t pid = fork();
-
-    if(pid == -1)
+    if(type == MSG_READDIR) 
     {
-        printf("%s\n", "Could not fork");
+        LOG("%s\n", "MSG_READDIR");
+        readdir_handler(client_fd, req_header);
         return;
     }
-    else if(pid == 0) 
+    else if(type == MSG_GETATTR)
     {
-        /* Child */
-        if(type == MSG_READDIR) 
-        {
-            LOG("%s\n", "MSG_READDIR");
-            readdir_handler(client_fd, req_header);
-            return;
-        }
-        else if(type == MSG_GETATTR)
-        {
-            LOG("%s\n", "MSG_GETATTR");
-            getattr_handler(client_fd, req_header);
-            return;
-        }
-        else if(type == MSG_OPEN)
-        {
-            LOG("%s\n", "MSG_OPEN");
-            open_handler(client_fd, req_header);
-            return;
-        }
-        else if(type == MSG_READ)
-        {
-            LOG("%s\n", "MSG_READ");
-            read_handler(client_fd, req_header);
-            return;
-        }
-        else 
-        {
-            LOG("%s\n", "error: Unknown request type\n"); 
-            return; 
-        }
+        LOG("%s\n", "MSG_GETATTR");
+        getattr_handler(client_fd, req_header);
+        return;
     }
-    wait(&status);
-
-    // if(type == MSG_READDIR) 
-    // {
-    //     LOG("%s\n", "MSG_READDIR");
-    //     readdir_handler(client_fd, req_header);
-    //     return;
-    // }
-    // else if(type == MSG_GETATTR)
-    // {
-    //     LOG("%s\n", "MSG_GETATTR");
-    //     getattr_handler(client_fd, req_header);
-    //     return;
-    // }
-    // else if(type == MSG_OPEN)
-    // {
-    //     LOG("%s\n", "MSG_OPEN");
-    //     open_handler(client_fd, req_header);
-    //     return;
-    // }
-    // else if(type == MSG_READ)
-    // {
-    //     LOG("%s\n", "MSG_READ");
-    //     read_handler(client_fd, req_header);
-    //     return;
-    // }
-    // else 
-    // {
-    //     LOG("%s\n", "error: Unknown request type\n");  
-    // }
+    else if(type == MSG_OPEN)
+    {
+        LOG("%s\n", "MSG_OPEN");
+        open_handler(client_fd, req_header);
+        return;
+    }
+    else if(type == MSG_READ)
+    {
+        LOG("%s\n", "MSG_READ");
+        read_handler(client_fd, req_header);
+        return;
+    }
+    else 
+    {
+        LOG("%s\n", "error: Unknown request type\n"); 
+        return; 
+    }
 
     return;
 
 }
 
+/*
+ * Scan through a directory and transmit them across the network to the client
+ */
 void readdir_handler(int client_fd, struct netfs_msg_header req_header) 
 {
     char path[MAXIMUM_PATH] = { 0 };
     read_len(client_fd, path, req_header.msg_len);
-    LOG("readdir: %s\n", path);
+    LOG("READDIR: %s\n", path);
 
     char full_path[MAXIMUM_PATH] = { 0 };
     strcpy(full_path, ".");
@@ -137,6 +102,7 @@ void readdir_handler(int client_fd, struct netfs_msg_header req_header)
         return;
     }
 
+    // Write each directory entry
     uint16_t len;
     struct dirent *entry;
     while ((entry = readdir(directory)) != NULL) 
@@ -144,7 +110,6 @@ void readdir_handler(int client_fd, struct netfs_msg_header req_header)
         len = strlen(entry->d_name) + 1;
         write_len(client_fd, &len, sizeof(uint16_t));
         write_len(client_fd, entry->d_name, len);
-        // printf("-> %s\n", entry->d_name);
     }
 
     // Last directory entry
@@ -153,15 +118,23 @@ void readdir_handler(int client_fd, struct netfs_msg_header req_header)
 
     closedir(directory);
     close(client_fd); // Close socket connection
-    // fflush(stdout);
     return;
 }
 
+/*
+ * Transmit the resulting struct directly over the network
+ */
 void getattr_handler(int client_fd, struct netfs_msg_header req_header) 
 {
+    struct passwd *pw_server;
+    uid_t uid;
+
+    uid = geteuid();
+    pw_server = getpwuid(uid);
+
     char path[MAXIMUM_PATH] = { 0 };
     read_len(client_fd, path, req_header.msg_len);
-    LOG("getattr: %s\n", path);
+    LOG("GETATTR: %s\n", path);
 
     // Return if at root directory
     if(strcmp(path, "/") == 0)
@@ -174,22 +147,37 @@ void getattr_handler(int client_fd, struct netfs_msg_header req_header)
     struct stat stbuf;
     struct attr_stat atst = {0};
 
+    int stat_success = 0;
     if(stat(full_path, &stbuf) < 0)
     {
         LOG("%s\n", "Stat function failed");
+        write_len(client_fd, &stat_success, sizeof(int));
         close(client_fd);
         return;
     }
+    else
+    {
+        stat_success = 1;
+        LOG("%s\n", "Stat function success");
+        write_len(client_fd, &stat_success, sizeof(int));
+    }
 
-    printf("\n***IS FILE***\n\n");
+    // Add attributes to custom struct
+    LOG("\n%s\n\n", "***IS FILE***");
     atst.ino = stbuf.st_ino;
-    atst.uid = stbuf.st_uid;
+    atst.uid = stbuf.st_uid; 
     atst.gid = stbuf.st_gid;
     atst.mode = stbuf.st_mode;
     atst.nlink = stbuf.st_nlink;
     atst.size = stbuf.st_size;
     atst.blocks = stbuf.st_blocks;
     atst.mtim = stbuf.st_mtim;
+
+    // Check if server id matches file id
+    if(pw_server->pw_uid == atst.uid)
+        atst.uid = 1;
+    else
+        atst.uid = 0;
 
     // Change permissions to read-only
     printf("File information for %s before change\n", path);
@@ -210,35 +198,14 @@ void getattr_handler(int client_fd, struct netfs_msg_header req_header)
     int statchmod = atst.mode & (S_IRWXU | S_IRWXG | S_IRWXO);
     printf("********chmod: %o\n", statchmod);
 
-    if((atst.mode >> 7) & 0b1)
-    {
-        if(S_ISDIR(atst.mode))
-            atst.mode = atst.mode & (S_IFDIR | 0577);
-        else
-            atst.mode = atst.mode & (S_IFREG | 0577);
-    }
-    if((atst.mode >> 4) & 0b1)
-    {
-        if(S_ISDIR(atst.mode))
-            atst.mode = atst.mode & (S_IFDIR | 0757);
-        else
-            atst.mode = atst.mode & (S_IFREG | 0757);
-    }
-    if((atst.mode >> 1) & 0b1)
-    {
-        if(S_ISDIR(atst.mode))
-            atst.mode = atst.mode & (S_IFDIR | 0775);
-        else
-            atst.mode = atst.mode & (S_IFREG | 0775);
-    }
+    // Change bits to read-only
+    if(S_ISDIR(atst.mode))
+        atst.mode = atst.mode & (S_IFDIR | 0555);
+    else
+        atst.mode = atst.mode & (S_IFREG | 0555);
 
-    // if(S_ISDIR(atst.mode))
-    //     atst.mode = S_IFDIR | 0444;
-    // if(S_ISREG(atst.mode))
-    //     atst.mode = S_IFREG | 0444;
-
-    // atst.mode = atst.mode & 0555;
-    // atst.mode = S_IFREG & 0555;
+    statchmod = atst.mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+    printf("********chmod: %o\n\n", statchmod);
 
     printf("File information for %s after change\n", path);
     printf("---------------------------\n");
@@ -255,44 +222,21 @@ void getattr_handler(int client_fd, struct netfs_msg_header req_header)
     printf( (atst.mode & S_IXOTH) ? "x" : "-");
     printf("\n\n");
 
+    // Write custom struct to client
     write_len(client_fd, &atst, sizeof(struct attr_stat));
 
     close(client_fd); // Close socket connection
-    // fflush(stdout);
-
-    
-    // printf("HERE3\n");
-    // printf( (S_ISDIR(atst->mode)) ? "d" : "-");
-
-    // if(S_ISREG(stbuf->st_mode))
-    // {
-        // printf("IS FILE\n");
-        // atst->ino = stbuf->st_ino;
-        // atst->uid = stbuf->st_uid;
-        // atst->gid = stbuf->st_gid;
-        // atst->mode = stbuf->st_mode;
-        // atst->nlink = stbuf->st_nlink;
-        // atst->size = stbuf->st_size;
-        // write_len(client_fd, atst, sizeof(struct attr_stat));
-    //     close(client_fd);
-    // }
-    // else if(S_ISDIR(stbuf->st_mode))
-    // {
-    //     printf("IS DIRC\n");
-    //     write_len(client_fd, atst, sizeof(struct attr_stat));
-    //     close(client_fd);
-    // }
-
-
-    
     return;
 }
 
+/*
+ * Opens file given and sends to file descriptor to client
+ */
 void open_handler(int client_fd, struct netfs_msg_header req_header)
 {
     char path[MAXIMUM_PATH] = { 0 };
     read_len(client_fd, path, req_header.msg_len);
-    LOG("open: %s\n", path);
+    LOG("OPEN: %s\n", path);
 
     char full_path[MAXIMUM_PATH] = { 0 };
     strcpy(full_path, ".");
@@ -301,6 +245,8 @@ void open_handler(int client_fd, struct netfs_msg_header req_header)
     uint16_t success;
 
     /* Check here if read_handler problems occur */
+
+    // Open file
     int fd = open(full_path, O_RDONLY);
     if(fd == -1)
     {
@@ -316,85 +262,92 @@ void open_handler(int client_fd, struct netfs_msg_header req_header)
         write_len(client_fd, &success, sizeof(uint16_t));
     }
 
-    // Send file descriptor for read
-    write_len(client_fd, &fd, sizeof(int));
-
     close(client_fd);
-    // fflush(stdout);
     return;
 }
 
+/*
+ * Receives file information and and send file data with sendfile()
+ */
 void read_handler(int client_fd, struct netfs_msg_header req_header)
 {
-    int fd;
+    // int fd;
     size_t size;
     off_t offset;
 
-    // fd
-    read_len(client_fd, &fd, sizeof(int));
+    char path[MAXIMUM_PATH] = { 0 };
+    read_len(client_fd, path, req_header.msg_len);
+    LOG("READ: %s\n", path);
+
+    char full_path[MAXIMUM_PATH] = { 0 };
+    strcpy(full_path, ".");
+    strcat(full_path, path);
+
+    struct stat stbuf;
+    struct attr_stat atst = {0};
+
+    int stat_success = 0;
+    if(stat(full_path, &stbuf) < 0)
+    {
+        LOG("%s\n", "Stat function failed");
+        write_len(client_fd, &stat_success, sizeof(int));
+        close(client_fd);
+        return;
+    }
+    else
+    {
+        stat_success = 1;
+        LOG("%s\n", "Stat function success");
+        write_len(client_fd, &stat_success, sizeof(int));
+    }
+
+    int bytes_read = stbuf.st_size;
+
+    // Open file
+    int fd = open(full_path, O_RDONLY);
+
     // size
     read_len(client_fd, &size, sizeof(size_t));
     // offset
     read_len(client_fd, &offset, sizeof(off_t));
 
-
-    // for(size_t size_to_send = size; size_to_send > 0; )
-    // {
-    //     ssize_t sent = sendfile(client_fd, fd, &offset, size_to_send);
-    //     if(sent <= 0)
-    //     {
-    //         if(sent != 0)
-    //             perror("sendfile");
-    //         break;
-    //     }
-
-    //     offset += sent;
-    //     size_to_send -= sent;
-    // }
-
     // Create buffer with size of file
-    char *buf = malloc(sizeof(char) * size);
+    // char *buf = malloc(sizeof(char) * size);
     // Returns the number of bytes read, zero indicates end of file
-    int success = pread(fd, buf, size, offset);
+    // int bytes_read = pread(fd, buf, size, offset);
+    // printf("bytes read: %d\n", bytes_read);
 
-    write_len(client_fd, &success, sizeof(int));
+    write_len(client_fd, &bytes_read, sizeof(int));
     
-    if(success >= 0)
+    if(bytes_read > 0)
     {
-        // write_len(client_fd, buf, sizeof(char*));
-        for(size_t size_to_send = size; size_to_send > 0; )
+        while(size > 0)
         {
-            ssize_t sent = sendfile(client_fd, fd, &offset, size_to_send);
+            ssize_t sent = sendfile(client_fd, fd, &offset, size);
+            printf("Sent: %d\n", sent);
+            printf("Offset: %lld\n", offset);
             if(sent <= 0)
             {
                 if(sent != 0)
-                    perror("sendfile");
+                    perror("Read failed");
+
                 break;
             }
-
-            offset += sent;
-            size_to_send -= sent;
+            size -= sent;
         }
     }
 
     close(client_fd);
-    // fflush(stdout);
     return;
 }
 
 int main(int argc, char *argv[]) 
 {
-
-    /* This starter code will initialize the server port and wait to receive a
-     * message. */
-
-    // chdir to the directory youre going to serve 
-    /* TODO: Check start of path if it is home */
+    // Change to directory provided
     chdir(argv[1]);
-    home_path = argv[1];
-
+    // Set port 
     int port = DEFAULT_PORT;
-    if(strcmp(argv[2], ".") != 0)
+    if(argc == 3)
         port = atoi(argv[2]);
 
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -419,7 +372,24 @@ int main(int argc, char *argv[])
 
     LOG("Listening on port %d\n", port);
 
+    // Limit the maximum number of processes to avoid overwhelming the system
+    int num_processes = 0;
+    int max_processes = 4;
+
     while(true) {
+
+        if (num_processes < max_processes) {
+            int wstat;
+            int ret = waitpid(-1, &wstat, WNOHANG);
+            if (ret > 0) {
+                num_processes--;
+            }
+        } else {
+            int wstat;
+            wait(&wstat);
+            num_processes--;
+        }
+
         struct sockaddr_storage client_addr = { 0 };
         socklen_t slen = sizeof(client_addr);
 
@@ -442,12 +412,20 @@ int main(int argc, char *argv[])
                 sizeof(remote_host));
         LOG("Accepted connection from %s\n", remote_host);
 
-        handle_request(client_fd);
+        pid_t pid = fork();
+        num_processes++;
+        if(pid == 0) 
+        {
+            handle_request(client_fd);
+            close(client_fd);  
+        }
 
-        // TODO: going to have to fork for other parts of P3
-        
+
+        // close(client_fd);  
 
     }
+
+    // close(socket_fd);
 
     return 0;
 
